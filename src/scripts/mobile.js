d504e71c972917426929fbe4aab6ba5349ef4794
Maia.th <thiagomaia633@gmail.com>
2024-06-26 18:39:30 -0300

    feat: Funcao para fechar o menu mobile adicionada

--- src/scripts/mobile.js
+++ src/scripts/mobile.js
@@ -1,13 +1,27 @@
 function menuShow() {
   let menuArea = document.getElementById("Menu-Open");
-  let menuMobile = document.getElementById("Mobile")
-  menuMobile.classList.toggle('ativo');
-  
-  if (menuArea.style.width == "50vw") {
+  let menuMobile = document.getElementById("Mobile");
+
+  // Alternar a classe 'ativo' no menu mobile
+  menuMobile.classList.toggle("ativo");
+
+  if (menuArea.style.width === "50vw") {
     menuArea.style.width = "0vh";
     menuArea.style.display = "none";
+    document.removeEventListener("click", closeMenuOnClickOutside);
   } else {
     menuArea.style.width = "50vw";
     menuArea.style.display = "block";
+    setTimeout(() => {
+      document.addEventListener("click", closeMenuOnClickOutside);
+    }, 0); // Adiciona o listener após a abertura do menu
+  }
+}
+
+function closeMenuOnClickOutside(event) {
+  let menuArea = document.getElementById("Menu-Open");
+
+  if (!menuArea.contains(event.target) && !menuArea.contains(event.target)) {
+    menuShow();
   }
 }
